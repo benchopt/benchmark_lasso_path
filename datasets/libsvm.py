4e0592acb1872e3969f419083ed699173a85fced
Johan Larsson <johanlarsson@outlook.com>
2022-04-21 15:33:42 +0200

    feat: add libsvm data sets

---
+++ datasets/libsvm.py
@@ -0,0 +1,29 @@
+from benchopt import BaseDataset, safe_import_context
+
+with safe_import_context() as import_ctx:
+    from libsvmdata import fetch_libsvm
+
+
+class Dataset(BaseDataset):
+
+    name = "libsvm"
+
+    parameters = {
+        "dataset": ["bodyfat", "leukemia", "rcv1.binary"],
+    }
+
+    install_cmd = "conda"
+    requirements = ["pip:libsvmdata"]
+
+    def __init__(self, dataset="bodyfat"):
+        self.dataset = dataset
+        self.X, self.y = None, None
+
+    def get_data(self):
+
+        if self.X is None:
+            self.X, self.y = fetch_libsvm(self.dataset)
+
+        data = dict(X=self.X, y=self.y)
+
+        return self.X.shape[1], data
