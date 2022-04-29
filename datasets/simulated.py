a03e7cc0aa61e6a9c381eb99d4646c4768fdd4bb
Johan Larsson <johanlarsson@outlook.com>
2022-04-29 16:05:58 +0200

    feat: use more appropriate data simulation

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -1,30 +1,30 @@
-import numpy as np
 from benchopt import BaseDataset
+from benchopt.datasets.simulated import make_correlated_data
 
 
 class Dataset(BaseDataset):
 
     name = "Simulated"
 
-    # List of parameters to generate the datasets. The benchmark will consider
-    # the cross product for each key in the dictionary.
-    parameters = {"n_samples, n_features": [(5_000, 200), (200, 10_000)]}
+    parameters = {
+        "n_samples, n_features": [(5_000, 200), (200, 5_000)],
+        "rho": [0, 0.5],
+    }
 
-    def __init__(self, n_samples=10, n_features=50, random_state=27):
-        # Store the parameters of the dataset
+    def __init__(self, n_samples=10, n_features=50, rho=0, random_state=27):
         self.n_samples = n_samples
         self.n_features = n_features
         self.random_state = random_state
+        self.rho = rho
 
     def get_data(self):
+        X, y, _ = make_correlated_data(
+            self.n_samples,
+            self.n_features,
+            rho=self.rho,
+            random_state=self.random_state,
+        )
 
-        rng = np.random.RandomState(self.random_state)
-        X = rng.randn(self.n_samples, self.n_features)
-        y = rng.randn(self.n_samples)
-
-        # `data` holds the keyword arguments for the `set_data` method of the
-        # objective.
-        # They are customizable.
         data = dict(X=X, y=y)
 
         return self.n_features, data
