ec6d843dc885cf002f7514c58722f6e617915cf5
Cassio F. Dantas <cassiofragadantas@gmail.com>
2022-05-05 14:26:18 +0200

    - fit_intercept support for celer solver

--- solvers/celer.py
+++ solvers/celer.py
@@ -3,8 +3,11 @@ import warnings
 from benchopt import BaseSolver, safe_import_context
 
 with safe_import_context() as import_ctx:
+    from numpy import vstack
+    from scipy import sparse
     from celer import celer_path
     from sklearn.exceptions import ConvergenceWarning
+    from sklearn.linear_model._base import _preprocess_data
 
 
 class Solver(BaseSolver):
@@ -20,29 +23,48 @@ class Solver(BaseSolver):
     ]
 
     def set_objective(self, X, y, lambdas, fit_intercept):
+        # celer/sklearn way of handling intercept: center X and y for dense
+        if fit_intercept:
+            X, y, X_offset, y_offset, _ = _preprocess_data(
+                X, y, fit_intercept, return_mean=True
+            )
+            self.X_offset = X_offset
+            self.y_offset = y_offset
+
         self.X, self.y = X, y
         self.lambdas = lambdas
         self.fit_intercept = fit_intercept
 
-    def skip(self, X, y, lambdas, fit_intercept):
-        if fit_intercept:
-            return True, f"{self.name} does not handle fit_intercept"
-
-        return False, None
-
     def run(self, tol):
         warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
-        _, self.coefs, _ = celer_path(
-            self.X,
-            self.y,
-            pb="lasso",
-            alphas=self.lambdas / len(self.y),
-            prune=1,
-            tol=tol,
-            max_iter=1_000,
-            max_epochs=100_000,
-        )
+        if self.fit_intercept and sparse.issparse(self.X):
+            _, self.coefs, _ = celer_path(
+                self.X,
+                self.y,
+                pb="lasso",
+                alphas=self.lambdas / len(self.y),
+                prune=1,
+                tol=tol,
+                max_iter=1_000,
+                max_epochs=100_000,
+                X_offset=self.X_offset,
+            )
+        else:
+            _, self.coefs, _ = celer_path(
+                self.X,
+                self.y,
+                pb="lasso",
+                alphas=self.lambdas / len(self.y),
+                prune=1,
+                tol=tol,
+                max_iter=1_000,
+                max_epochs=100_000,
+            )
+
+        if self.fit_intercept:
+            intercept = self.y_offset - self.X_offset @ self.coefs
+            self.coefs = vstack((self.coefs, intercept))
 
     def get_result(self):
         return self.coefs
