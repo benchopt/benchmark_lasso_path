2c5e46ea3e4d6a03932dd1e3245d07d63d5ca247
Johan Larsson <johanlarsson@outlook.com>
2022-06-20 12:17:10 +0200

    chore: format with black

--- solvers/gsroptim.py
+++ solvers/gsroptim.py
@@ -1,21 +1,19 @@
-from benchopt import BaseSolver
-from benchopt import safe_import_context
+from benchopt import BaseSolver, safe_import_context
 
 with safe_import_context() as import_ctx:
     import warnings
-    from sklearn.exceptions import ConvergenceWarning
-    from scipy import sparse
+
     from gsroptim.lasso import lasso_path
+    from scipy import sparse
+    from sklearn.exceptions import ConvergenceWarning
 
 
 class Solver(BaseSolver):
-    name =  "gsroptim"
+    name = "gsroptim"
     stopping_strategy = "iteration"
 
     install_cmd = "conda"
-    requirements = [
-        'pip:git+https://github.com/EugeneNdiaye/Gap_Safe_Rules.git@master'
-    ]
+    requirements = ["pip:git+https://github.com/EugeneNdiaye/Gap_Safe_Rules.git@master"]
     references = [
         "E. Ndiaye, O. Fercoq, A. Gramfort and J. Salmon, JMLR, "
         '"Gap Safe screening rules for sparsity enforcing penalties", '
@@ -29,8 +27,14 @@ class Solver(BaseSolver):
 
     def run(self, n_iter):
         warnings.filterwarnings("ignore", category=ConvergenceWarning)
-        self.coefs = lasso_path(self.X, self.y, self.lambdas, eps=1e-12,
-                                max_iter=n_iter, fit_intercept=self.fit_intercept)[1]
+        self.coefs = lasso_path(
+            self.X,
+            self.y,
+            self.lambdas,
+            eps=1e-12,
+            max_iter=n_iter,
+            fit_intercept=self.fit_intercept,
+        )[1]
 
     def get_result(self):
         return self.coefs
