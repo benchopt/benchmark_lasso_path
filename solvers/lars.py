cf09288d6d18b480c8d20a5e9a52d8bba9c13e61
Johan Larsson <johanlarsson@outlook.com>
2022-06-20 12:09:08 +0200

    fix: import INFINITY from stopping_criterion
    The definition of INFINITY has moved.

--- solvers/lars.py
+++ solvers/lars.py
@@ -1,6 +1,5 @@
 from benchopt import BaseSolver, safe_import_context
-from benchopt.runner import INFINITY
-from benchopt.stopping_criterion import SufficientProgressCriterion
+from benchopt.stopping_criterion import SufficientProgressCriterion, INFINITY
 
 with safe_import_context() as import_ctx:
     import warnings
