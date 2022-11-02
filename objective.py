3e08e48da09790bc213ccede11bc49d119676772
Johan Larsson <13087841+jolars@users.noreply.github.com>
2022-11-02 16:40:25 +0100

    refactor: use glmnet instead of sklearn for path (#20)

--- objective.py
+++ objective.py
@@ -3,20 +3,23 @@ from benchopt import BaseObjective, safe_import_context
 with safe_import_context() as import_ctx:
     import numpy as np
     from numpy.linalg import norm
-    from sklearn.linear_model import Lasso
+
+    select_lambdas = import_ctx.import_from("utils", "select_lambdas")
 
 
 class Objective(BaseObjective):
     name = "Lasso Path"
+
+    install_cmd = "conda"
+    requirements = ["r-base", "rpy2", "r-glmnet", "r-matrix"]
 
     parameters = {
         "fit_intercept": [True, False],
         "n_lambda": [100],
     }
 
-    def __init__(self, fit_intercept=False, n_lambda=100, lambda_min_ratio=None):
+    def __init__(self, fit_intercept=False, n_lambda=100):
         self.fit_intercept = fit_intercept
-        self.lambda_min_ratio = lambda_min_ratio
         self.n_lambda = n_lambda
 
     def _get_lambda_max(self):
@@ -28,64 +31,7 @@ class Objective(BaseObjective):
     def set_data(self, X, y):
         self.X, self.y = X, y
         self.n_samples, self.n_features = X.shape
-
-        # the following code fits the path once using the same path-stopping
-        # criteria that glmnet uses to find a reasonable lambda path in order
-        # to obtain a reasonable lambda sequence
-        if self.lambda_min_ratio is None:
-            self.lambda_min_ratio = 1e-2 if self.n_samples < self.n_features else 1e-4
-
-        lambda_max = self._get_lambda_max()
-        lambdas = np.geomspace(
-            lambda_max, lambda_max * self.lambda_min_ratio, self.n_lambda
-        )
-
-        # the pmax setting is redundant because dfmax is hardcoded, but it's
-        # kept just to be explicit about how this is done
-        dfmax = self.n_features + 1
-        pmax = min(dfmax * 2 + 20, self.n_features)
-
-        lasso = Lasso(
-            max_iter=10000,
-            alpha=lambda_max / self.n_samples,
-            # TODO(jolars): consider whether tol might be too high
-            tol=1e-4,
-            fit_intercept=self.fit_intercept,
-            warm_start=True,
-        )
-
-        r = y - np.mean(y) if self.fit_intercept else y.copy()
-        null_dev = 0.5 * np.linalg.norm(r) ** 2
-        dev_prev = null_dev
-
-        i = 0
-        ever_active = np.array([])
-
-        for i, lam in enumerate(lambdas):
-            lasso.alpha = lam / self.n_samples
-            lasso.fit(X, y)
-
-            w = lasso.coef_
-            intercept = lasso.intercept_
-
-            dev = 0.5 * np.linalg.norm(y - X @ w - intercept) ** 2
-
-            dev_ratio = 1 - dev / null_dev
-            dev_change = 1 - dev / dev_prev
-            dev_prev = dev
-
-            n_nonzero = np.sum(w != 0)
-            ever_active = np.union1d(ever_active, np.where(w != 0))
-
-            if (
-                n_nonzero >= dfmax
-                or dev_ratio > 0.999
-                or (i > 0 and dev_change < 1e-5)
-                or len(ever_active) >= pmax
-            ):
-                break
-
-        self.lambdas = lambdas[: i + 1]
+        self.lambdas = select_lambdas(X, y, self.fit_intercept)
 
     def get_one_solution(self):
         return np.zeros([self.n_features, len(self.lambdas)])
--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,17 @@
 from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
+    import numpy as np
+    from benchopt.helpers.r_lang import import_rpackages
+    from rpy2 import robjects
+    from rpy2.robjects import numpy2ri, packages
     from scipy import sparse
     from sklearn.feature_selection import VarianceThreshold
     from sklearn.preprocessing import MaxAbsScaler, StandardScaler
+
+    # Setup the system to allow rpy2 running
+    numpy2ri.activate()
+    import_rpackages("glmnet")
 
 
 def preprocess_data(X, y=None):
@@ -15,3 +23,31 @@ def preprocess_data(X, y=None):
         X = StandardScaler().fit_transform(X)
 
     return X, y
+
+
+def select_lambdas(X, y, fit_intercept):
+    # run glmnet once to obtain a lambda sequence
+    if sparse.issparse(X):
+        r_Matrix = packages.importr("Matrix")
+        X_coo = X.tocoo()
+        X_tmp = r_Matrix.sparseMatrix(
+            i=robjects.IntVector(X_coo.row + 1),
+            j=robjects.IntVector(X_coo.col + 1),
+            x=robjects.FloatVector(X_coo.data),
+            dims=robjects.IntVector(X_coo.shape),
+        )
+    else:
+        X_tmp = robjects.r.matrix(X, X.shape[0], X.shape[1])
+
+    y_tmp = robjects.FloatVector(y)
+    glmnet = robjects.r["glmnet"]
+    glmnet_fit = glmnet(
+        X_tmp,
+        y_tmp,
+        intercept=fit_intercept,
+        standardize=False,
+        maxit=1_000_000,
+    )
+    results = dict(zip(glmnet_fit.names, list(glmnet_fit)))
+
+    return np.array(results["lambda"]) * len(y)
