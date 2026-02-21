4f7df78eae3be76aa51b6ee84955fcf32b455ab8
tommoral <thomas.moreau.2010@gmail.com>
2026-02-21 22:05:27 +0100

    FIX xfail tests failing for good reason (no dataset avail/non maintained package)

--- test_config.py
+++ test_config.py
@@ -1,13 +1,22 @@
-import sys  # noqa: F401
-
-import pytest  # noqa: F401
+import pytest
 
 
-def check_test_solver_install(solver_class):
+def check_test_dataset_get_data(benchmark, dataset_class):
+    """Hook to skip `test_dataset_get_data` for specific datasets."""
+    if dataset_class.name == "breheny":
+        pytest.xfail(
+            "breheny dataset is not available anymore on S3--gives 403 error."
+        )
+
+
+def check_test_solver_install(benchmark, solver_class):
     """Hook called in `test_solver_install`.
 
     If one solver needs to be skip/xfailed on some
     particular architecture, call pytest.xfail when
     detecting the situation.
     """
-    pass
+    if solver_class.name == "gsroptim":
+        pytest.xfail(
+            "gsroptim is not compatible with pip 26.0+."
+        )
