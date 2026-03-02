1621f67d99a1ff6cfc2f1a85214d81fb2ea97687
Thomas Moreau <thomas.moreau.2010@gmail.com>
2026-03-02 23:08:25 +0100

    Apply suggestion from @tomMoral

--- solvers/lars.py
+++ solvers/lars.py
@@ -20,7 +20,6 @@ class Solver(BaseSolver):
     )
 
     install_cmd = "conda"
-    requirements = ["scikit-learn>=1.7.0"]
     references = [
         "B. Efron, T. Hastie, I. Johnstone, R. Tibshirani"
         '"Least Angle Regression", Annals of Statistics, '
